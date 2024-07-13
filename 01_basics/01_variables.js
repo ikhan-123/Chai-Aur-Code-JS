e9b2c85d237f274d3fe8e37d1cbf9bd7d5d12a7a
ikhan-123 <ifficodder@gmail.com>
2024-07-13 12:11:34 +0500

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 1122
+let accountEmail = "irfan@yahoo.com"
+var accountPassword = "8938"
+accountId = "Karachi"
+
+console.table([accountId , accountEmail , accountPassword  ])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict";  // treat all Js code as newer version
+// console.log("hello world");
+
+// alert( 3 + 3)  //we are using nodejs, not browser
+
+// number =>  2 to power 53 
+// bigint
+// string => ""
+// boolean  => true/false
+// null => standalone  value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined);
