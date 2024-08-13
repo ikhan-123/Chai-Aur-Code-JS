f1e5900765b1ba11ca07ee6093fbba9c9d609a05
ikhan-123 <ifficodder@gmail.com>
2024-08-13 16:36:54 +0500

    Operations

--- 01_basics/03_conversionsOperation.js
+++ 01_basics/03_conversionsOperation.js
@@ -23,8 +23,36 @@ console.log(booleanisLoggedIn);
 // "" =>false
 //"Irfan"  => true
 
-let someNumber = 32
+// let someNumber = 32
 
-let stringNumber =  String(someNumber)
-console.log(typeof stringNumber);
+// let stringNumber =  String(someNumber)
+// console.log(typeof stringNumber);
+
+//  ********************** Operations ***********************
+
+let value = 3
+let negValue = -value 
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = " Irfan"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+ 
 
