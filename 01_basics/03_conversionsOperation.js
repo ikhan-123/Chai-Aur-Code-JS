57e4634b6c729b922869181193a914c600aec2d3
ikhan-123 <ifficodder@gmail.com>
2024-08-13 12:22:26 +0500

    Conversion of DataTypes

---
+++ 01_basics/03_conversionsOperation.js
@@ -0,0 +1,30 @@
+let score = "32abc"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+
+// "32"  => 32
+//"32abc" => NaN
+// true => 1; false => 0;
+
+let isloggedIn = "Irfan"
+
+let booleanisLoggedIn = Boolean (isloggedIn)
+console.log(booleanisLoggedIn);
+
+// 1 => true; 0 => false
+// "" =>false
+//"Irfan"  => true
+
+let someNumber = 32
+
+let stringNumber =  String(someNumber)
+console.log(typeof stringNumber);
+
