883817eb02014ce6b81e4c72286470439ac662fa
ikhan-123 <ifficodder@gmail.com>
2024-08-19 17:20:38 +0500

    Array part1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,59 @@
+// Array
+
+const myArr = [1,2,3,4,5,6,7,8,9];
+const myHeros = ["spiderm=Man", "Iron Man", "BataMan"]
+
+// const myArr2 = new Array (1,3,5,7)
+
+// console.log(myArr2);
+
+
+// Array Methods
+// 1. push() - adds an element to the end of an array
+myArr.push(6)
+// console.log(myArr);
+
+// 2. pop() - removes the last element of an array
+myArr.pop()
+// console.log(myArr);
+
+// 3. shift() - removes the first element of an array
+myArr.shift()
+// console.log(myArr);
+
+// 4. unshift() - adds an element to the beginning of an array
+myArr.unshift(0)
+// console.log(myArr);
+
+const newArr = myArr.join()
+// console.log(myArr);
+// console.log(newArr);
+
+
+// slice or Splice
+// 1. slice() - returns a shallow copy of a portion of an array
+
+
+
+// console.log("A ", myArr);
+
+
+const myn1 = myArr.slice(0,4);
+// console.log(myn1);
+// console.log("B ", myArr);
+
+// 2. splice() - Method modifies the original array by removing or replacing elements, and returns the removed elements.
+myArr.splice
+const myn2 = myArr.splice(0,4)
+// console.log("C ", myArr);
+console.log(myn2);
+
+
+
+
+
+
+
+
+
+
