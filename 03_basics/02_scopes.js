460e4f770a5337a598597cd1e027fb61a5f57673
ikhan-123 <ifficodder@gmail.com>
2024-08-28 15:54:51 +0500

    scopelevel and minihoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -9,16 +9,64 @@ if (true) {
     let a = 10
     const b = 20
     var c = 30
-    console.log("INNER: " ,a);
+    // console.log("INNER: " ,a);
     
 }
 
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
     
-}
+// }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one (){
+    const username = "irfan"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+        
+    }
+    // console.log(website);
+
+    // two()
+    
+}
+
+// one()
+
+
+if(true){
+    const username = "Irfan Khan"
+    if(username === "Irfan Khan"){
+        const website = "youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+    // console.log(username);
+}
+
+
+
+// ********************** Interesting ****************************
+
+
+console.log(addOne(5))
+
+function addOne (num){
+    return num + 1
+}
+
+
+
+const addTwo = function (num){
+    return num + 2
+}
+console.log(addTwo(5));
